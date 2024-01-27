bbed50c704187ab4a354ad0afd19bf905d15aaef
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-27 10:08:45 +0700

    Seventh

--- romantic_alexa.py
+++ romantic_alexa.py
@@ -12,26 +12,33 @@ engine.setProperty('voice', voices[1].id)
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-    
+
 # Function to capture user's voice command
 def take_command():
     try:
-    # Using a microphone as the audio source
+        # Using a microphone as the audio source
         with sr.Microphone() as source:
             print('listening...')
             voice = listener.listen(source)
-        # Converting voice to text using Google's speech recognition
+            # Converting voice to text using Google's speech recognition
             command = listener.recognize_google(voice)
             command = command.lower()
-    # Removing the wake word "alexa" from the command
+             # Removing the wake word "alexa" from the command
             if 'alexa' in command:
                 command = command.replace('alexa', '')
                 print(command)
     except:
         pass
+    return command
+
 # Function to execute different commands based on user input
-# Getting user's command
-# Performing actions based on recognized command
+def run_alexa():
+    # Getting user's command
+    command = take_command()
+    print(command)
+    # Performing actions based on recognized command
+    if 'play' in command:
+        song = command.replace('play', '')
 # Getting information about a person from Wikipedia
 # Getting a joke using the pyjokes library
 # Main loop to keep Alexa running and listening for commands
